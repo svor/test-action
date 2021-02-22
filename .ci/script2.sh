a47158b6fa0e121b9227f20bd7ca4a43a5d94c10
svor <vsvydenk@redhat.com>
2021-02-22 19:15:15 +0200

    1
    Signed-off-by: svor <vsvydenk@redhat.com>

---
+++ .ci/script2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shellcheck disable=SC1091,SC1090
+#
+# Copyright (c) 2012-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+echo "Hello 1"
